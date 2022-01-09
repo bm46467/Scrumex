40836a6027f3c90387ce87547ab0b426262b141a
adi <adrian.tworek.it@gmail.com>
2022-01-09 22:51:51 +0100

    Add validation to Login and Register components

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,41 @@
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { object, string } from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import { LockClosedIcon } from '@heroicons/react/solid'
 
+const schema = object({
+  email: string()
+    .required('Email is required')
+    .email('Please provide a valid email address'),
+  password: string()
+    .required('Password is required')
+    .min(8, 'Password must contain at least 8 characters'),
+})
+
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = (values) => {
+    console.log(values)
+
+    reset()
+  }
+
   return (
     <form
       className="max-w-[290px] flex flex-col items-center border-2 
     border-gray-500 rounded-xl mx-auto mt-24 mb-12 p-8 gap-4 shadow-md"
       autoComplete="off"
+      onSubmit={handleSubmit(onSubmit)}
     >
       <LockClosedIcon className="h-7 text-indigo-400" />
 
@@ -17,48 +45,42 @@ const Login = () => {
         <input
           id="email"
           name="email"
-          className="peer w-full h-11 border-b-2 bg-transparent border-gray-400
-          focus:outline-none focus:border-indigo-600 placeholder-transparent"
+          className={`peer w-full h-11 border-b-2 bg-transparent border-gray-400
+          focus:outline-none focus:border-indigo-600 placeholder-transparent transition 
+          ${errors?.email?.message && 'focus:border-red-800'}`}
           type="text"
           placeholder="Email address"
+          {...register('email')}
         />
-        <label
-          htmlFor="email"
-          className="absolute left-0 -top-3.5 text-gray-400 transition-all 
-           peer-placeholder-shown:text-base
-           peer-placeholder-shown:text-gray-200
-           peer-placeholder-shown:top-2
-           peer-focus:-top-3.5
-           peer-focus:text-gray-400
-           peer-focus:text-sm
-           "
-        >
+        <label htmlFor="email" className="form-label">
           Email address
         </label>
+
+        <span className="text-red-800 inline-block pt-2">
+          {errors?.email?.message}
+        </span>
       </div>
 
       <div className="relative mt-2 w-full">
         <input
           id="password"
           name="password"
-          className="peer w-full h-11 border-b-2 bg-transparent border-gray-400
-          focus:outline-none focus:border-indigo-600 placeholder-transparent"
+          className={`peer w-full h-11 border-b-2 bg-transparent border-gray-400
+          focus:outline-none focus:border-indigo-600 transition ${
+            errors?.password?.message && 'focus:border-red-800'
+          }
+          placeholder-transparent`}
           type="password"
           placeholder="Password"
+          {...register('password')}
         />
-        <label
-          htmlFor="password"
-          className="absolute left-0 -top-3.5 text-gray-400 transition-all 
-           peer-placeholder-shown:text-base
-           peer-placeholder-shown:text-gray-200
-           peer-placeholder-shown:top-2
-           peer-focus:-top-3.5
-           peer-focus:text-gray-400
-           peer-focus:text-sm
-           "
-        >
+        <label htmlFor="password" className="form-label">
           Password
         </label>
+
+        <span className="text-red-800 inline-block pt-2">
+          {errors?.password?.message}
+        </span>
       </div>
 
       <button className="btn-primary--filled mt-6 px-12">Login</button>
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,14 +1,58 @@
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { object, string, ref } from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import { LockClosedIcon } from '@heroicons/react/solid'
 
+const nameRegex =
+  /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
+const capitalizeRegex = /^[A-ZÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð]/
+
+const schema = object({
+  firstName: string()
+    .required('First name is required')
+    .matches(capitalizeRegex, 'First letter must be uppercase')
+    .matches(nameRegex, 'First name is invalid'),
+  lastName: string()
+    .required('Last name is required')
+    .matches(capitalizeRegex, 'First letter must be uppercase')
+    .matches(nameRegex, 'Last name is invalid'),
+  email: string()
+    .required('Email address is required')
+    .email('Please provide a valid email address'),
+  password: string()
+    .required('Password is required')
+    .min(8, 'Password must contain at least 8 characters'),
+  passwordConfirm: string()
+    .required('Password confirmation is required')
+    .min(8, 'Password must contain at least 8 characters')
+    .oneOf([ref('password'), null], 'Passwords must match'),
+})
+
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = (values) => {
+    console.log(values)
+
+    reset()
+  }
+
   return (
     <div className="pb-32">
       <form
         className="max-w-[290px] flex flex-col items-center border-2 
     border-gray-500 rounded-xl mx-auto mt-24 p-8 gap-4 shadow-md"
         autoComplete="off"
+        onSubmit={handleSubmit(onSubmit)}
       >
         <LockClosedIcon className="h-7 text-indigo-400" />
 
@@ -19,48 +63,40 @@ const Register = () => {
             <input
               id="firstName"
               name="firstName"
-              className="peer w-full h-11 border-b-2 bg-transparent border-gray-400
-          focus:outline-none focus:border-indigo-600 placeholder-transparent"
+              className={`peer w-full h-11 border-b-2 bg-transparent border-gray-400
+              ${errors?.firstName?.message && 'focus:border-red-800'}
+              focus:outline-none focus:border-indigo-600 placeholder-transparent`}
               type="text"
               placeholder="First name"
+              {...register('firstName')}
             />
-            <label
-              htmlFor="firstName"
-              className="absolute left-0 -top-3.5 text-gray-400 transition-all 
-           peer-placeholder-shown:text-base
-           peer-placeholder-shown:text-gray-200
-           peer-placeholder-shown:top-2
-           peer-focus:-top-3.5
-           peer-focus:text-gray-400
-           peer-focus:text-sm
-           "
-            >
+            <label htmlFor="firstName" className="form-label">
               First name
             </label>
+
+            <span className="text-red-800 inline-block pt-2">
+              {errors?.firstName?.message}
+            </span>
           </div>
 
           <div className="relative mt-2 w-full">
             <input
               id="lastName"
               name="lastName"
-              className="peer w-full h-11 border-b-2 bg-transparent border-gray-400
-          focus:outline-none focus:border-indigo-600 placeholder-transparent"
+              className={`peer w-full h-11 border-b-2 bg-transparent border-gray-400
+              ${errors?.lastName?.message && 'focus:border-red-800'}
+              focus:outline-none focus:border-indigo-600 placeholder-transparent`}
               type="text"
               placeholder="Last name"
+              {...register('lastName')}
             />
-            <label
-              htmlFor="lastName"
-              className="absolute left-0 -top-3.5 text-gray-400 transition-all 
-           peer-placeholder-shown:text-base
-           peer-placeholder-shown:text-gray-200
-           peer-placeholder-shown:top-2
-           peer-focus:-top-3.5
-           peer-focus:text-gray-400
-           peer-focus:text-sm
-           "
-            >
+            <label htmlFor="lastName" className="form-label">
               Last name
             </label>
+
+            <span className="text-red-800 inline-block pt-2">
+              {errors?.lastName?.message}
+            </span>
           </div>
         </div>
 
@@ -68,72 +104,60 @@ const Register = () => {
           <input
             id="email"
             name="email"
-            className="peer w-full h-11 border-b-2 bg-transparent border-gray-400
-          focus:outline-none focus:border-indigo-600 placeholder-transparent"
+            className={`peer w-full h-11 border-b-2 bg-transparent border-gray-400
+            ${errors?.email?.message && 'focus:border-red-800'}
+            focus:outline-none focus:border-indigo-600 placeholder-transparent`}
             type="text"
             placeholder="Email address"
+            {...register('email')}
           />
-          <label
-            htmlFor="email"
-            className="absolute left-0 -top-3.5 text-gray-400 transition-all 
-           peer-placeholder-shown:text-base
-           peer-placeholder-shown:text-gray-200
-           peer-placeholder-shown:top-2
-           peer-focus:-top-3.5
-           peer-focus:text-gray-400
-           peer-focus:text-sm
-           "
-          >
+          <label htmlFor="email" className="form-label">
             Email address
           </label>
+
+          <span className="text-red-800 inline-block pt-2">
+            {errors?.email?.message}
+          </span>
         </div>
 
         <div className="relative mt-2 w-full">
           <input
             id="password"
             name="password"
-            className="peer w-full h-11 border-b-2 bg-transparent border-gray-400
-          focus:outline-none focus:border-indigo-600 placeholder-transparent"
+            className={`peer w-full h-11 border-b-2 bg-transparent border-gray-400
+            ${errors?.password?.message && 'focus:border-red-800'}
+            focus:outline-none focus:border-indigo-600 placeholder-transparent`}
             type="password"
             placeholder="Password"
+            {...register('password')}
           />
-          <label
-            htmlFor="password"
-            className="absolute left-0 -top-3.5 text-gray-400 transition-all 
-           peer-placeholder-shown:text-base
-           peer-placeholder-shown:text-gray-200
-           peer-placeholder-shown:top-2
-           peer-focus:-top-3.5
-           peer-focus:text-gray-400
-           peer-focus:text-sm
-           "
-          >
+          <label htmlFor="password" className="form-label">
             Password
           </label>
+
+          <span className="text-red-800 inline-block pt-2">
+            {errors?.password?.message}
+          </span>
         </div>
 
         <div className="relative mt-2 w-full">
           <input
             id="passwordConfirm"
             name="passwordConfirm"
-            className="peer w-full h-11 border-b-2 bg-transparent border-gray-400
-          focus:outline-none focus:border-indigo-600 placeholder-transparent"
+            className={`peer w-full h-11 border-b-2 bg-transparent border-gray-400
+            ${errors?.passwordConfirm?.message && 'focus:border-red-800'}
+            focus:outline-none focus:border-indigo-600 placeholder-transparent`}
             type="password"
             placeholder="Confirm password"
+            {...register('passwordConfirm')}
           />
-          <label
-            htmlFor="passwordConfirm"
-            className="absolute left-0 -top-3.5 text-gray-400 transition-all 
-           peer-placeholder-shown:text-base
-           peer-placeholder-shown:text-gray-200
-           peer-placeholder-shown:top-2
-           peer-focus:-top-3.5
-           peer-focus:text-gray-400
-           peer-focus:text-sm
-           "
-          >
+          <label htmlFor="passwordConfirm" className="form-label">
             Confirm password
           </label>
+
+          <span className="text-red-800 inline-block pt-2">
+            {errors?.passwordConfirm?.message}
+          </span>
         </div>
 
         <button className="btn-primary--filled mt-6 px-12">Register</button>
