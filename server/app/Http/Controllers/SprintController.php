e9d6f4070edc2dc1706ef29dfde6812371109561
Adrian Tworek <adrian.tworek.it@gmail.com>
2022-06-18 17:06:12 +0200

    Changes

--- server/app/Http/Controllers/SprintController.php
+++ server/app/Http/Controllers/SprintController.php
@@ -16,37 +16,40 @@ class SprintController extends Controller
 
     public function getSpecificSprint($pid, $sid)
     {
-        return Project::find($pid)->sprints->where("id","=",$sid);
+        return Project::find($pid)->sprints->where("id", "=", $sid);
     }
 
-    public function getAllSprintTasks($sid){
+    public function getAllSprintTasks($sid)
+    {
         return Sprint::find($sid)->tasks;
     }
 
-    public function getSpecificSprintTask($pid, $sid, $tid){
-        return Sprint::find($sid)->tasks->where("id","=",$tid);
+    public function getSpecificSprintTask($pid, $sid, $tid)
+    {
+        return Sprint::find($sid)->tasks->where("id", "=", $tid);
     }
 
     public function store(Request $request)
     {
-        $request->validate([ 
+        $request->validate([
             'project_id' => 'required',
             'startDate' => 'required',
             'stopDate' => 'required',
             'interval' => 'required'
         ]);
-    
+
         return Sprint::create($request->all());
     }
 
-    public function deleteTaskFromSprint($pid, $sid, $tid){
+    public function deleteTaskFromSprint($pid, $sid, $tid)
+    {
         $res = Task::find($tid)->delete();
         return "deleted.";
     }
 
-    public function deleteSpecificSprint($pid, $sid){
+    public function deleteSpecificSprint($pid, $sid)
+    {
         $res = Sprint::find($sid)->delete();
         return "deleted.";
     }
-
 }
